4e1873d6591d4bcdfd88d0400846b3d441ad2602
markotomic <masheritto@gmail.com>
2023-02-14 19:47:05 +0100

    Update February 14, 2023 / 19:47

--- src/components/StationStatTrack.jsx
+++ src/components/StationStatTrack.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import Statf from "../functions/Statf";
 import Statif from "../functions/Statif";
+import Staticof from "../functions/Staticof";
 
 const StationStatTrack = ({ aqi }) => {
   return (
     <div
-      className={`w-full text-very-dark-grey p-5 ${Statf(
+      className={`w-full text-[18px] md:text-[22px] font-semibold text-very-dark-grey p-5 ${Statf(
         aqi
-      )} rounded-md flex justify-between gap-2`}
+      )} rounded-md flex justify-between items-center gap-2`}
     >
       <div className="flex md:flex-row flex-col gap-3 items-center">
-        <div className="bg-glass-black p-3 rounded-md flex justify-center md:w-auto w-full">
+        <div className="bg-glass-black p-5 rounded-md flex justify-center md:w-auto w-full">
           {aqi}
         </div>
         <div>{Statif(aqi)}</div>
       </div>
-      <div></div>
+      <div className="max-w-[80px]">
+        <img src={Staticof(aqi)} alt="" />
+      </div>
     </div>
   );
 };
---
+++ src/functions/Staticof.js
@@ -0,0 +1,24 @@
+import goodIcon from "../assets/good-icon.svg";
+import moderateIcon from "../assets/moderate-icon.svg";
+import ufsgIcon from "../assets/ufsg-icon.svg";
+import unhealthyIcon from "../assets/unhealthy-icon.svg";
+import vuhIcon from "../assets/vuh-icon.svg";
+import hazardousIcon from "../assets/hazardous-icon.svg";
+
+const Staticof = (aqi) => {
+  if (aqi >= 0 && aqi <= 50) {
+    return goodIcon;
+  } else if (aqi >= 51 && aqi <= 100) {
+    return moderateIcon;
+  } else if (aqi >= 101 && aqi <= 150) {
+    return ufsgIcon;
+  } else if (aqi >= 151 && aqi <= 200) {
+    return unhealthyIcon;
+  } else if (aqi >= 201 && aqi <= 300) {
+    return vuhIcon;
+  } else if (aqi > 300) {
+    return hazardousIcon;
+  }
+};
+
+export default Staticof;
