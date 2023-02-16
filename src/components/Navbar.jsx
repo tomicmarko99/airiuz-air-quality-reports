fb0ba73232141e7203a6a16b58dec34d791454e6
markotomic <masheritto@gmail.com>
2023-02-16 14:16:46 +0100

    Update / Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,11 +52,8 @@ const Navbar = () => {
           <Link to="/about" className="hover:text-white">
             About
           </Link>
-          <Link to="/" className="hover:text-white">
-            Blog
-          </Link>
-          <Link to="/" className="hover:text-white">
-            Resources
+          <Link to="/rankings" className="hover:text-white">
+            Rankings
           </Link>
           <Link to="/faqs" className="hover:text-white">
             FAQs
@@ -92,25 +89,11 @@ const Navbar = () => {
               About
             </Link>
             <Link
-              to="/"
+              to="/rankings"
               onClick={changeExpanded}
               className="flex gap-3 py-1 items-center hover:text-melon-white hover:bg-bgr-white px-5"
             >
-              Blog
-            </Link>
-            <Link
-              to="/"
-              onClick={changeExpanded}
-              className="flex gap-3 py-1 items-center hover:text-melon-white hover:bg-bgr-white px-5"
-            >
-              Resources
-            </Link>
-            <Link
-              to="/faqs"
-              onClick={changeExpanded}
-              className="flex gap-3 py-1 items-center hover:text-melon-white hover:bg-bgr-white px-5"
-            >
-              FAQs
+              Rankings
             </Link>
             <Link
               to="/privacy-policy"
@@ -121,11 +104,11 @@ const Navbar = () => {
             </Link>
             <div className="px-5 py-4 flex flex-col gap-3 border-y-2 border-glass-black mt-3">
               <Link
-                to="/"
+                to="/faqs"
                 onClick={changeExpanded}
                 className="border-2 border-soft-blue hover:border-medium-blue rounded-md w-full px-3 py-1 text-soft-blue hover:text-medium-blue text-center"
               >
-                Track AQI
+                FAQs
               </Link>
 
               <Link
